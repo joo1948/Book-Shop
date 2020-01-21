c762358a41071532183a26d55a6d3b7341e45183
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-01-22 00:01:04 +0900

    main

---
+++ src/main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.net.*;
+import javax.swing.*;
+import javax.swing.*;
+
+
+public class main extends JFrame{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			new Home();
+	}
+}
+ 
